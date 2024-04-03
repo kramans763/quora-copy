e90ddbc59a3baeabeac3cc354e0a8ae1c9bd48b9
Aman Singh <kramans763@gmail.com>
2024-04-03 22:48:38 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,43 +1,32 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware,compose } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import reducer from './Reducer';
-// import { composeWithDevTools } from "redux-devtools-extension";
 import { combineReducers } from "redux";
 import { BrowserRouter } from 'react-router-dom';
-import { thunk } from 'redux-thunk';
-// import thunkMiddleware from 'redux-thunk';
+import {thunk} from 'redux-thunk';
+
+
 
 const rootReducer = combineReducers({
-   reducer
-  });
+  reducer
+});
 
-// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-// const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 const store = createStore(rootReducer, compose(applyMiddleware(thunk)));
 
+// Use createRoot instead of ReactDOM.render
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
-ReactDOM.render(
+// Wrap your app inside a root element
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
-
-// root.render(
-//   <React.StrictMode>
-//     <Provider store={store}>
-//       <BrowserRouter>
-//         <App />
-//       </BrowserRouter>
-       
-//     </Provider>
-//   </React.StrictMode>
-// );
