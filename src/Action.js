2ec8259e67c916b76eef2acf5f010eab44c6408b
Aman Singh <kramans763@gmail.com>
2024-04-04 08:59:03 +0530

    update

--- src/Action.js
+++ src/Action.js
@@ -53,36 +53,3 @@ export const getChannels = () => {
   };
 };
 
-// export const getPosts= () =>{
-//     return async (dispatch) =>{
-//       const response =  await fetch('https://academics.newtonschool.co/api/v1/quora/post', {
-//           headers:  {
-//              'projectID': 'f104bi07c490'
-//           }
-//       })
-//       let data=await response.json();
-//       const posts = data.data || [];
-//       dispatch({
-//         type: 'GETPOSTS',
-//         posts: posts
-//       })
-//     }
-//   }
-  
-//   export const getChannels =()=>{
-//     return async (dispatch) =>{
-//       const response =  await fetch('https://academics.newtonschool.co/api/v1/quora/channel?limit=10', {
-//           headers:  {
-//              'projectID': 'f104bi07c490'
-//           }
-//       })
-//       let data=await response.json();
-//       const channels = data.data || [];
-//       dispatch({
-//         type: 'GETCHANNELS',
-//         channels: channels
-//       })
-//     }
-//   }
-  
-  
